28f6bf93a0e5f3b05574bbdf75ae00d7d0f51e33
Nate Tucker <ntuckertriplet@gmail.com>
2019-06-17 15:23:21 -0500

    some routing

--- app/app.py
+++ app/app.py
@@ -1,10 +1,13 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return "<h1>Hello World</h1>"
+    return render_template('index.html')
 
-@app.route('/flag')
-def flag():
-    return "<h1>cdc{url_injection}</h1>"
+@app.route('/<flag>')
+def flag(flag):
+    if flag is 'cdc{url_injection}':
+        return "<h1>%s</h1>" % flag
+    else:
+        return render_template('welcome.html')
