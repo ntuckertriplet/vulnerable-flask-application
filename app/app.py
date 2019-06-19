d66e3f9221551016d4b75cfaa6f5049c37e6a737
Nate Tucker <ntuckertriplet@gmail.com>
2019-06-19 08:23:18 -0500

    movin on over to linux

--- app/app.py
+++ app/app.py
@@ -1,16 +1,6 @@
 from flask import Flask, render_template, request
-import sqlalchemy
-from sqlalchemy import * 
 
 app = Flask(__name__)
-
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////mnt/Users/utuckn1/ISEAGE/vulnerable-flask/app/vulnerable.db'
-
-db = sqlalchemy(app)
-
-class User(db.Model):
-    name = db.Column(db.String(20))
-    password = db.Column(db.String(20))
 
 @app.route('/')
 def index():
@@ -22,3 +12,6 @@ def flag():
         return 'cdc{good_request}'
     else:
         return 'change your methods'
+
+if __name__=="__main__":
+    app.run()
